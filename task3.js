5c157c4716a01046cb710acebc10a3a699ab7609
prasanth <guruprasanth543@gmail.com>
2023-10-24 13:38:31 +0530

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const sumOfSquaredEvenNumbers = numbers
@@ -7,13 +6,3 @@ const sumOfSquaredEvenNumbers = numbers
   .reduce((acc, num) => acc + num, 0); 
 
 console.log("Sum of squared even numbers:", sumOfSquaredEvenNumbers);
-=======
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const sumOfSquaredEvenNumbers = numbers
-  .filter((num) => num % 2 === 0) // Filter even numbers
-  .map((num) => num * num) // Square each even number
-  .reduce((acc, num) => acc + num, 0); // Sum the squared numbers
-
-console.log("Sum of squared even numbers:", sumOfSquaredEvenNumbers);
->>>>>>> 753b0ae237c94e21e35649ce7e75954db52fa4d3
